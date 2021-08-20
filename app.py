4cfefa53064206713891f1fc190f5ccefb5edfc4
Ryan Cabell <cabell.ryan@gmail.com>
2021-08-20 19:31:40 -0400

    fixed image by adding "static" folder because flask wants to use a folder that didnt exist i guess

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-#Home page
+# Home page
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("base.html")
 
-#pass in page as var
+# pass in page as var
+
+
 @app.route('/<name>/')
 def user(name):
-    return render_template("index.html", content=["bulbsaur","ivysaur","venasaur", name])
+    return render_template("index.html", content=[name])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
