e0a54182ab180e6c6ef4877195eb5b134350210b
Ryan Cabell <cabell.ryan@gmail.com>
2021-09-06 16:37:28 -0400

    buggy login page w/navbar

--- app.py
+++ app.py
@@ -7,12 +7,26 @@ app = Flask(__name__)
 def home():
     return render_template("base.html")
 
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 # pass in page as var
 
 
-@app.route('/<name>/')
-def user(name):
-    return render_template("index.html", content=[name])
+# @app.route('/<name>/')
+# def user(name):
+#     return render_template("index.html", content=[name])
 
 
 if __name__ == "__main__":
