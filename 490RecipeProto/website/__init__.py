5bffeef7ab8661e001fc9eea6b447829b9d3ee92
Christopher Jackson <47529262+cdjacks5@users.noreply.github.com>
2021-11-03 12:22:58 -0400

    Add files via upload
    added file to prototype

---
+++ 490RecipeProto/website/__init__.py
@@ -0,0 +1,16 @@
+#create flask app
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hello'
+
+#register blueprints
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+    
+
+    return app
