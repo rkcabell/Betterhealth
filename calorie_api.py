4054110341e651bcd536608bfddd4ec37e83c407
Cassidy <c_deveno@uncg.edu>
2021-11-04 22:14:26 -0400

    testing cal page

--- calorie_api.py
+++ calorie_api.py
@@ -7,12 +7,7 @@ def calorie_calc(Ingredient_unformatted):
     response = requests.get('https://api.edamam.com/api/nutrition-data?app_id=fdb7ea11&app_key=09001dad57a62484bd663dab44500239&nutrition-type=cooking&ingr=' + _ingr)
     data = response.json()
     if data['calories'] == 0:
-        return("Invalid Entry: Check spelling and format")
-        #print("Don't forget to include ingredient amount")
+        #return("Invalid Entry: Check spelling and format")
+        return("Invalid")
     else:
-            # display calories to web page
-            # print("Calories: ")
             return  (data['calories'])
-            # save unformatted ingr to db
-            # print("String to save to db: ")
-            # print(Ingredient_unformatted)
